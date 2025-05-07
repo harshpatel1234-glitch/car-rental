d3aec4cf7f7e5074cd5ad13ad02020180079d5f5
harsh <hvpatel068@gmail.com>
2025-05-07 18:45:55 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,8 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export default clerkMiddleware()
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  response.headers.set("x-custom-header", "hello");
+  return response;
 }
